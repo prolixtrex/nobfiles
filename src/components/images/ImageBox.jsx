561e3f35c6ecab4a8a8d9bdddd3b2026542522f8
Bright Anua <73463821+prolixtrex@users.noreply.github.com>
2024-06-13 08:14:12 +0100

    Removed drag and drop

--- src/components/images/ImageBox.jsx
+++ src/components/images/ImageBox.jsx
@@ -1,30 +1,30 @@
-import { useContext } from "react";
-import { useDrag, useDrop } from "react-dnd";
-import { DataContext } from "../../dataContext/DataContext";
+// import { useContext } from "react";
+// import { useDrag, useDrop } from "react-dnd";
+// import { DataContext } from "../../dataContext/DataContext";
 import { Link } from "react-router-dom";
 
 const ImageBox = ({ src, index, moveImage, tag, title }) => {
-    const { loggedIn } = useContext(DataContext);
+    // const { loggedIn } = useContext(DataContext);
 
-    const [, ref] = useDrag({
-        type: "IMAGE",
-        item: { index },
-    });
+    // const [, ref] = useDrag({
+    //     type: "IMAGE",
+    //     item: { index },
+    // });
 
-    const [, drop] = useDrop({
-        accept: "IMAGE",
-        hover: (draggedImage) => {
-            if (draggedImage.index !== index) {
-                moveImage(draggedImage.index, index);
-                draggedImage.index = index;
-            }
-        },
-    });
+    // const [, drop] = useDrop({
+    //     accept: "IMAGE",
+    //     hover: (draggedImage) => {
+    //         if (draggedImage.index !== index) {
+    //             moveImage(draggedImage.index, index);
+    //             draggedImage.index = index;
+    //         }
+    //     },
+    // });
 
     return (
         <Link
             to={`/${title}`}
-            ref={(node) => loggedIn && ref(drop(node))}
+            // ref={(node) => loggedIn && ref(drop(node))}
             className="imageBox"
         >
             <img src={src} alt={index} draggable />
--- src/components/images/ImageContainer.jsx
+++ src/components/images/ImageContainer.jsx
@@ -1,22 +1,22 @@
 import { useContext } from "react";
 import { DataContext } from "../../dataContext/DataContext";
-import { DndProvider } from "react-dnd";
-import { HTML5Backend } from "react-dnd-html5-backend";
-import { TouchBackend } from "react-dnd-touch-backend";
+// import { DndProvider } from "react-dnd";
+// import { HTML5Backend } from "react-dnd-html5-backend";
+// import { TouchBackend } from "react-dnd-touch-backend";
 import Header from "../common/header/Header";
 import ImageBox from "./ImageBox";
 
 const ImageContainer = ({ tagName }) => {
-    const { photos, setPhotos } = useContext(DataContext);
+    const { photos } = useContext(DataContext);
     // const [isTouchDevice, setIsTouchDevice] = useState(false);
-    const isTouchDevice = "ontouchstart" in window;
+    // const isTouchDevice = "ontouchstart" in window;
 
-    const moveImage = (fromIndex, toIndex) => {
-        const updatedImages = [...photos];
-        const [movedImage] = updatedImages.splice(fromIndex, 1);
-        updatedImages.splice(toIndex, 0, movedImage);
-        setPhotos(updatedImages);
-    };
+    // const moveImage = (fromIndex, toIndex) => {
+    //     const updatedImages = [...photos];
+    //     const [movedImage] = updatedImages.splice(fromIndex, 1);
+    //     updatedImages.splice(toIndex, 0, movedImage);
+    //     setPhotos(updatedImages);
+    // };
 
     // useEffect(()=> {
     //     const detectDevice = () => {
@@ -25,23 +25,23 @@ const ImageContainer = ({ tagName }) => {
     // })
 
     return (
-        <DndProvider backend={isTouchDevice ? TouchBackend : HTML5Backend}>
-            <div className="imageContainer">
-                <Header />
-                <div className="images">
-                    {photos.map((photo, index) => (
-                        <ImageBox
-                            key={index}
-                            moveImage={moveImage}
-                            index={index}
-                            src={photo.src}
-                            title={photo.title}
-                            tag={photo.tag}
-                        />
-                    ))}
-                </div>
+        // <DndProvider backend={isTouchDevice ? TouchBackend : HTML5Backend}>
+        <div className="imageContainer">
+            <Header />
+            <div className="images">
+                {photos.map((photo, index) => (
+                    <ImageBox
+                        key={index}
+                        // moveImage={moveImage}
+                        index={index}
+                        src={photo.src}
+                        title={photo.title}
+                        tag={photo.tag}
+                    />
+                ))}
             </div>
-        </DndProvider>
+        </div>
+        // </DndProvider>
     );
 };
 
